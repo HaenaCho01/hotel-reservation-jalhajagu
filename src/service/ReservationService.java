98ebb252d11c70cc7c645d107312aa8894d5eb5b
Eunbi Kim <eunbkim4@gmail.com>
2023-06-07 16:11:31 +0900

    refactor: 고객 엔티티 생성자 및 메서드 추가

--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -21,26 +21,22 @@ public class ReservationService {
     public void addReservation(int roomNumber, Customer customer, LocalDate startDate, LocalDate endDate) {
         //매개변수 LocalDate 타입인지 확실하지 않음.
         Room room = hotelService.findRoom(roomNumber);
-
-        // 아래 4개 줄은 임의로 작성했습니다. 변경해주세요!!!!!!!
-        String confirmationDate = ""; // 현재 시간을 UTC 포맷으로
-        Period period = Period.between(startDate, endDate);
-        Reservation reservation = new Reservation(room, customer.getName(), customer.getPhoneNumber(), confirmationDate, period);
-        reservationMap.put(reservation.getId(), reservation);
-        System.out.println("객실 예약이 성공적으로 완료되었습니다.");
-
         int roomPrice = room.getPrice();
-        makePayment(customer, roomPrice);
-        System.out.println("숙박 금액이 결제되었습니다.");
-    }
 
-    private void makePayment(Customer customer, int roomPrice) {
-        // 고객 소지금에서 숙박비 제하고...
-        int updatedMoney = customer.getMoney() - roomPrice;
-        customer.setMoney(updatedMoney);
+        if(customer.canAfford(roomPrice)) {
+            // 아래 4개 줄은 임의로 작성했습니다. 변경해주세요!!!!!!!
+            String confirmationDate = ""; // 현재 시간을 UTC 포맷으로
+            Period period = Period.between(startDate, endDate);
+            Reservation reservation = new Reservation(room, customer.getName(), customer.getPhoneNumber(), confirmationDate, period);
+            reservationMap.put(reservation.getId(), reservation);
+            System.out.println("객실 예약이 성공적으로 완료되었습니다.");
 
-        // 호텔 매출에 반영
-        hotelService.addToTotalSales(roomPrice);
+            customer.makePayment(roomPrice);
+            hotelService.addToTotalSales(roomPrice);
+            System.out.println("숙박 금액이 결제되었습니다.");
+        } else {
+            System.out.println("숙박 금액이 소지금보다 많아 예약이 불가합니다.");
+        }
     }
 
     public void cancelReservation(Customer customer, String id) {
@@ -49,17 +45,9 @@ public class ReservationService {
         reservationMap.remove(id);
         System.out.println("해당 예약이 성공적으로 취소되었습니다.");
 
-        refund(customer, roomPrice);
-        System.out.println("숙박 금액이 환불되었습니다.");
-    }
-
-    private void refund(Customer customer, int roomPrice) {
-        // 고객 소지금에서 숙박비 더하고...
-        int updatedMoney = customer.getMoney() + roomPrice;
-        customer.setMoney(updatedMoney);
-
-        // 호텔 매출에 반영
+        customer.getRefund(roomPrice);
         hotelService.subtractFromTotalSales(roomPrice);
+        System.out.println("숙박 금액이 환불되었습니다.");
     }
 
     public void checkAllReservations() {
